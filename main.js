518ab411408760124d041efb8f42a898cfe1f4a3
Sandro Roth <sandroroth@outlook.com>
2021-07-04 16:08:15 +0200

    Implement basic game

--- main.js
+++ main.js
@@ -1,6 +1,77 @@
-import './style.css'
+import Alpine from "alpinejs";
+import "./style.scss";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+Alpine.directive(
+  "interval",
+  (_, { value, expression }, { cleanup, evaluateLater }) => {
+    const run = evaluateLater(expression);
+    const interval = setInterval(() => run(), value || 1000);
+    cleanup(() => clearInterval(interval));
+  }
+);
+
+Alpine.data("game", () => ({
+  template: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  numbers: [],
+  target: 10,
+  minCount: 2,
+  maxCount: 3,
+  maxLength: 10,
+  selectedIdx: [],
+  score: 0,
+  status: "INIT",
+  get currentTarget() {
+    return this.selectedIdx
+      .map((idx) => this.numbers[idx])
+      .reduce((acc, no) => acc + no, 0);
+  },
+  addNumber() {
+    this.numbers.push(
+      this.template[Math.floor(Math.random() * this.template.length)]
+    );
+  },
+  reset() {
+    this.selectedIdx = [];
+    this.numbers = [];
+    this.score = 0;
+    this.status = "PLAYING";
+  },
+  validate() {
+    if (
+      this.currentTarget < this.target &&
+      this.selectedIdx.length < this.maxCount &&
+      this.numbers.length <= this.maxLength
+    ) {
+      // Still playing
+      return;
+    }
+
+    this.status = "LOST";
+  },
+  checkTarget() {
+    if (
+      this.currentTarget === this.target &&
+      this.selectedIdx.length >= this.minCount &&
+      this.selectedIdx.length <= this.maxCount &&
+      this.numbers.length <= this.maxLength
+    ) {
+      this.score++;
+      this.removeSelection();
+    }
+  },
+  selectIndex(idx, toggle = false) {
+    if (!this.selectedIdx.includes(idx)) {
+      this.selectedIdx.push(idx);
+    } else if (toggle) {
+      this.selectedIdx = this.selectedIdx.filter(item => item !== idx);
+    }
+  },
+  removeSelection() {
+    this.numbers = this.numbers.filter(
+      (_, idx) => !this.selectedIdx.includes(idx)
+    );
+    this.selectedIdx.length = 0;
+  },
+}));
+
+Alpine.start();
